29093bc717982f92ad6866997f7e63fa1f32a8e3
Jesse Shawl <jesse@jesse.sh>
2023-02-14 16:11:01 -0600

    remove unused code

--- content_scripts/inject.js
+++ content_scripts/inject.js
@@ -4,16 +4,16 @@
   let speed = 100;
 
   browser.runtime.onMessage.addListener((message) => {
-    start = message.form.start
-    finish = message.form.finish
-    speed = message.form.speed
+    start = message.form.start;
+    finish = message.form.finish;
+    speed = message.form.speed;
   });
 
   setInterval(() => {
-    const video = document.querySelector("video")
-    video.playbackRate = speed / 100
-    if(video.currentTime > finish){
-      video.currentTime = start
+    const video = document.querySelector("video");
+    video.playbackRate = speed / 100;
+    if (video.currentTime > finish) {
+      video.currentTime = start;
     }
   }, 500);
-})()
+})();
--- popup/app.js
+++ popup/app.js
@@ -1,11 +1,9 @@
-browser.tabs
-.executeScript({ file: "/content_scripts/inject.js" })
-.then((e) => {
-  console.log(e)
-  document.addEventListener('click', () => {
-    browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
+browser.tabs.executeScript({ file: "/content_scripts/inject.js" }).then((e) => {
+  console.log(e);
+  document.addEventListener("click", () => {
+    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
       browser.tabs.sendMessage(tabs[0].id, {
-        form: Object.fromEntries(new FormData(document.querySelector("form")))
+        form: Object.fromEntries(new FormData(document.querySelector("form"))),
       });
     });
   });
